713cc3f21927e0bde7ec682cb4f52e20ff0cbd8a
gmyasoedov <gmyasoedov@yandex.ru>
2025-04-11 16:31:08 +0300

    chore: added param for filtering agents

--- src/test/java/io/github/grisha9/PatchAgentPathsTest.java
+++ src/test/java/io/github/grisha9/PatchAgentPathsTest.java
@@ -6,14 +6,15 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PatchAgentPathsTest {
 
     @Test
     public void testSingleAgent() {
         String commandLine = "java -javaagent:/home/agent1/declarative-patcher-1.0-SNAPSHOT.jar -jar test.jar";
-        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")));
+        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")), "");
         assertEquals(1, agentJarPaths.size());
         assertTrue(agentJarPaths.get(0).contains("declarative-patcher-1.0-SNAPSHOT.jar"));
     }
@@ -21,7 +22,16 @@ public class PatchAgentPathsTest {
     @Test
     public void testMultyAgents() {
         String commandLine = "java -javaagent:/home/agent/agent1.jar -Dparam=1 -javaagent:/home/agent/agent2.jar -jar test.jar";
-        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")));
+        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")), "");
+        assertEquals(2, agentJarPaths.size());
+        assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent1.jar")));
+        assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent2.jar")));
+    }
+
+    @Test
+    public void testMultyAgentsWithCleanUp() {
+        String commandLine = "java -javaagent:/home/agent/agent1.jar -Dparam=1 -javaagent:/home/agent/agent2.jar=32927 -jar test.jar";
+        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")), "");
         assertEquals(2, agentJarPaths.size());
         assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent1.jar")));
         assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent2.jar")));
@@ -31,9 +41,26 @@ public class PatchAgentPathsTest {
     public void testMultyAgentsInOneParama() {
         String commandLine = "java -javaagent:/home/agent/agent1.jar" + File.pathSeparator
                 + "/home/agent/agent2.jar -jar test.jar";
-        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")));
+        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")), "");
         assertEquals(2, agentJarPaths.size());
         assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent1.jar")));
+        assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent2.jar")));
+    }
+
+    @Test
+    public void testMultyAgentsFilter() {
+        String commandLine = "java -javaagent:/home/agent/agent1.jar -Dparam=1 -javaagent:/home/agent/agent2.jar -jar test.jar";
+        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")), "agent1");
+        assertEquals(1, agentJarPaths.size());
+        assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent1.jar")));
+    }
+
+    @Test
+    public void testMultyAgentsInOneParamaFilter() {
+        String commandLine = "java -javaagent:/home/agent/agent1.jar" + File.pathSeparator
+                + "/home/agent/agent2.jar -jar test.jar";
+        List<String> agentJarPaths = PatchAgentPremain.getAgentJarPaths(Arrays.asList(commandLine.split(" ")), "2.j");
+        assertEquals(1, agentJarPaths.size());
         assertTrue(agentJarPaths.stream().anyMatch(it -> it.contains("agent2.jar")));
     }
 }
